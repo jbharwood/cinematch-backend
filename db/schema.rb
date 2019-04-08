2ac4209f7d35b6bf8fe4a131aebc09344f05eece
jbharwood <josephblakeharwood@gmail.com>
2019-04-08 10:25:08 -0400

    fixed feeds bug

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,6 @@ ActiveRecord::Schema.define(version: 20190406154611) do
 
   create_table "feeds", force: :cascade do |t|
     t.string "name"
-    t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
