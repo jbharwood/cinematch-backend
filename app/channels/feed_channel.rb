0a606375d66074abf6699f1fd023139bde28bb5c
jbharwood <josephblakeharwood@gmail.com>
2019-04-12 12:22:39 -0400

    fixed chatbox

--- app/channels/feed_channel.rb
+++ app/channels/feed_channel.rb
@@ -1,10 +1,14 @@
 class FeedChannel < ApplicationCable::Channel
   def subscribed
     stream_from "feed_channel"
+    puts "something bigggggggggggggggggggggggg"
+    # var subscription = App.cable.subscriptions.create({channel: "FeedChannel", room_id: 1});
   end
 
   def unsubscribed
     puts "unsub"
+    x = Feed.find(params[:id])
+    x.unsubscribe()
     # Any cleanup needed when channel is unsubscribed
   end
 end
